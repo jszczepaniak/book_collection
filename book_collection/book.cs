03218148450d98444428155397dd5e91ec9955c0
Jarosław Szczepaniak <jarek.szczepaniak@gmail.com>
2016-08-14 22:44:07 +0200

    First

---
+++ book_collection/book.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace book_collection
+{
+    [Serializable()]
+    class book
+    {
+
+        int bookId;
+        string title;
+        DateTime year;
+        int authorId;
+        string isbn;
+
+        public book()
+        {
+
+        }
+        public book(int cBookId, string cTitle, DateTime cYear, int cAuthorId, string cIsbn)
+        {
+            bookId = cBookId;
+            title = cTitle;
+            cYear = year;
+            authorId = cAuthorId;
+            isbn = cIsbn;
+        }
+
+    }
+}
